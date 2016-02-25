444d0e8259b3e896dda97e8468b7568c5d69d578
sole <sole@mozilla.com>
2016-02-25 13:47:52 +0000

    try pkg for users

--- setup.js
+++ setup.js
@@ -1,3 +1,13 @@
 var chalk = require('chalk');
+var linuxUser = require('linux-user');
 
 console.log(chalk.black(chalk.bgYellow('*** pastelito lazy installer FTW ***')));
+
+addWheelGroup();
+
+
+function addWheelGroup() {
+	linuxUser.getGroups(function(err, info) {
+		console.log(info);
+	});
+}
