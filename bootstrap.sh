549916f8e7fbf9a97ad0e66290c7d6baf6ffe886
sole <sole@mozilla.com>
2016-02-25 15:13:03 +0000

    not upgrading just in case something goes bananas

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,2 @@
-apt-get update -y && apt-get upgrade -y && apt-get install git build-essential -y && curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash - && apt-get install -y nodejs && git clone https://github.com/sole/pastelito.git && cd pastelito && npm install && node setup.js
+# apt-get update -y && apt-get upgrade -y && 
+apt-get install git build-essential -y && curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash - && apt-get install -y nodejs && git clone https://github.com/sole/pastelito.git && cd pastelito && npm install && node setup.js
