a5bdbcfb47c6587bc4e73846b6b0c0a0a9856125
sole <sole@mozilla.com>
2016-02-26 13:05:41 +0000

    with babel transpiling

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ## megafast
 
+In Ubuntu desktop or with normal (sudoer) user
+
+```bash
+sudo su -
+```
+
+Enter password, then:
+
 ```bash
 curl -sL https://raw.githubusercontent.com/sole/pastelito/master/bootstrap.sh | bash -
 ```
--- setup.js
+++ setup.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 var chalk = require('chalk');
 var linuxUser = require('linux-user');
 
@@ -6,8 +8,23 @@ console.log(chalk.black(chalk.bgYellow('*** pastelito lazy installer FTW ***')))
 addWheelGroup();
 
 
-function addWheelGroup() {
-	linuxUser.getGroups(function(err, info) {
-		console.log(info);
+async function addWheelGroup() {
+	//linuxUser.getGroups(function(err, info) {
+	//	console.log(info);
+	//});
+
+	var groups = await getGroups();
+	console.log(groups);
+}
+
+async function getGroups() {
+	return new Promise(function(resolve, reject) {
+		linuxUser.getGroups(function(err, info) {
+			if(err) {
+				reject(err);
+			} else {
+				resolve(info);
+			}
+		});
 	});
 }
