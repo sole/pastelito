7162bd7a9dc471259d95059ce70b07e8d27c331c
sole <sole@mozilla.com>
2016-02-26 18:01:00 +0000

    make sudoers

--- setup.js
+++ setup.js
@@ -1,22 +1,39 @@
 import 'babel-polyfill';
 
+var fs = require('fs');
 var chalk = require('chalk');
+var shelljs = require('shelljs');
 var linuxUser = require('linux-user');
 
 console.log(chalk.black(chalk.bgYellow('*** pastelito lazy installer FTW ***')));
 
-addWheelGroup();
+var groupName = 'wheel';
 
+main().then(function() {
+	console.log(chalk.blue('fin'));
+}).catch(function(e) {
+	console.error(chalk.red('AAAH'), e);
+});
 
-async function addWheelGroup() {
+// ---
+
+async function main() {
+	await addGroupIfNotExists(groupName);
+	await makeSudoers(groupName);
+}
+
+// Add wheel group
+
+async function addGroupIfNotExists(name) {
+	console.log('add group', name);
 	var groups = await getGroups();
-	var name = 'wheel';
 	if(findGroup(groups, name) === undefined) {
 		console.log('didnt find', name, 'adding');
 		await addGroup(name);
 	} else {
 		console.log(name, 'already exists');
 	}
+	return true;
 }
 
 async function getGroups() {
@@ -47,5 +64,27 @@ async function addGroup(name) {
 				resolve(info);
 			}
 		});
+	});
+}
+
+// Edit sudoers
+
+async function makeSudoers(groupName) {
+	return new Promise(function(res, rej) {
+		console.log('make sudoers', groupName);
+		var file = '/etc/sudoers';
+		var str = '%' + groupName + ' ALL=(ALL) NOPASSWD:ALL';
+		var fileContents = fs.readFileSync(file);
+
+		var position = fileContents.indexOf(str);
+		
+		if(position === -1) {
+			console.log('adding', str, 'to', file);
+			fs.appendFileSync(file, '\n' + str + '\n', 'utf8');
+		} else {
+			console.log(groupName, 'is already a sudoer');
+		}
+
+		res();
 	});
 }
