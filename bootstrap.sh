541992c6f8be3be973a8e7c8c8b141df4b209c94
sole <sole@mozilla.com>
2016-03-02 13:12:44 +0000

    use correct compile and run commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
 # apt-get update -y && apt-get upgrade -y # commented out because it gets ubuntu desktop in a weird state
-apt-get install git build-essential openssh-server -y && curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - && apt-get install -y nodejs && git clone https://github.com/sole/pastelito.git && cd pastelito && npm install && node setup.js
+apt-get install git build-essential openssh-server -y && curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - && apt-get install -y nodejs && git clone https://github.com/sole/pastelito.git && cd pastelito && npm install && npm run compile && npm run run
